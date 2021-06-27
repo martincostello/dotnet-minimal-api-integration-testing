00d4dd6ac3156a759e36e8a3f9b60374c7e84dde
martincostello <martin@martincostello.com>
2021-06-27 17:22:42 +0100

    Add Razor page layouts
    Add Razor page layouts, rather than just a single all-in-one.

---
+++ src/TodoApp/Pages/Shared/Error.cshtml.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Martin Costello, 2021. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TodoApp.Pages
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class ErrorModel : PageModel
+    {
+        public string RequestId { get; set; } = default!;
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
