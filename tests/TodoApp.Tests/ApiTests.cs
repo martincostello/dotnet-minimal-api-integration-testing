4811198087616d915fe13ecb9f4e1b65e9079b07
martincostello <martin@martincostello.com>
2021-07-04 14:40:01 +0100

    Add test with TestServer
    Add a test for the API that uses TestServer and have the HttpServerFixture derive from that.

---
+++ tests/TodoApp.Tests/ApiTests.cs
@@ -0,0 +1,134 @@
+﻿// Copyright (c) Martin Costello, 2021. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TodoApp.Models;
+
+namespace TodoApp
+{
+    [Collection(TodoAppCollection.Name)]
+    public class ApiTests
+    {
+        public ApiTests(TodoAppFixture fixture, ITestOutputHelper outputHelper)
+        {
+            Fixture = fixture;
+            OutputHelper = outputHelper;
+            Fixture.SetOutputHelper(OutputHelper);
+        }
+
+        private TodoAppFixture Fixture { get; }
+
+        private ITestOutputHelper OutputHelper { get; }
+
+        [Fact]
+        public async Task Can_Manage_Todo_Items_With_Api()
+        {
+            // Arrange
+            var client = await CreateAuthenticatedClientAsync();
+
+            // Act - Get all the items
+            var items = await client.GetFromJsonAsync<TodoListViewModel>("/api/items");
+
+            // Assert - There should be no items
+            items.ShouldNotBeNull();
+            items.Items.ShouldNotBeNull();
+
+            var beforeCount = items.Items.Count;
+
+            // Arrange
+            var text = "Buy eggs";
+            var newItem = new CreateTodoItemModel { Text = text };
+
+            // Act - Add a new item
+            using var createdResponse = await client.PostAsJsonAsync("/api/items", newItem);
+
+            // Assert - An item was created
+            createdResponse.StatusCode.ShouldBe(HttpStatusCode.Created);
+            createdResponse.Headers.Location.ShouldNotBeNull();
+
+            using var createdStream = await createdResponse.Content.ReadAsStreamAsync();
+            using var createdJson = await JsonDocument.ParseAsync(createdStream);
+
+            // Arrange - Get the new item's URL and Id
+            var itemUri = createdResponse.Headers.Location;
+            var itemId = createdJson.RootElement.GetProperty("id").GetString();
+
+            // Act - Get the item
+            var item = await client.GetFromJsonAsync<TodoItemModel>(itemUri);
+
+            // Assert - Validate the item was created correctly
+            item.ShouldNotBeNull();
+            item.Id.ShouldBe(itemId);
+            item.IsCompleted.ShouldBeFalse();
+            item.LastUpdated.ShouldNotBeNull();
+            item.Text.ShouldBe(text);
+
+            // Act - Mark the item as being completed
+            using var completedResponse = await client.PostAsJsonAsync(itemUri + "/complete", new { });
+
+            // Assert - The item was completed
+            completedResponse.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+
+            item = await client.GetFromJsonAsync<TodoItemModel>(itemUri);
+
+            item.ShouldNotBeNull();
+            item.Id.ShouldBe(itemId);
+            item.Text.ShouldBe(text);
+            item.IsCompleted.ShouldBeTrue();
+
+            // Act - Get all the items
+            items = await client.GetFromJsonAsync<TodoListViewModel>("/api/items");
+
+            // Assert - The item was completed
+            items.ShouldNotBeNull();
+            items.Items.ShouldNotBeNull();
+            items.Items.Count.ShouldBe(beforeCount + 1);
+            item = items.Items.Last();
+
+            item.ShouldNotBeNull();
+            item.Id.ShouldBe(itemId);
+            item.Text.ShouldBe(text);
+            item.IsCompleted.ShouldBeTrue();
+            item.LastUpdated.ShouldNotBeNull();
+
+            // Act - Delete the item
+            using var deletedResponse = await client.DeleteAsync(itemUri);
+
+            // Assert - The item no longer exists
+            deletedResponse.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+
+            items = await client.GetFromJsonAsync<TodoListViewModel>("/api/items");
+
+            items.ShouldNotBeNull();
+            items.Items.ShouldNotBeNull();
+            items.Items.Count.ShouldBe(beforeCount);
+            items.Items.ShouldNotContain(p => p.Id == itemId);
+
+            var exception = await Assert.ThrowsAsync<HttpRequestException>(
+                () => client.GetFromJsonAsync<TodoItemModel>(itemUri));
+
+            exception.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+        }
+
+        private async Task<HttpClient> CreateAuthenticatedClientAsync()
+        {
+            var options = new WebApplicationFactoryClientOptions();
+            var client = Fixture.CreateClient(options);
+
+            var parameters = Array.Empty<KeyValuePair<string?, string?>>();
+            using var content = new FormUrlEncodedContent(parameters);
+
+            // Go through the sign-in flow, which will set
+            // the authentication cookie on the HttpClient.
+            using var response = await client.PostAsync("/signin", content);
+
+            response.IsSuccessStatusCode.ShouldBeTrue();
+
+            return client;
+        }
+    }
+}
--- tests/TodoApp.Tests/HttpServerFixture.cs
+++ tests/TodoApp.Tests/HttpServerFixture.cs
@@ -1,52 +1,24 @@
 ﻿// Copyright (c) Martin Costello, 2021. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.IO;
 using System.Net.Http;
-using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
-using AspNet.Security.OAuth.GitHub;
-using JustEat.HttpClientInterception;
-using MartinCostello.Logging.XUnit;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Hosting.Server.Features;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace TodoApp
 {
-    public sealed class HttpServerFixture : WebApplicationFactory<Startup>, IAsyncLifetime, ITestOutputHelperAccessor
+    public sealed class HttpServerFixture : TodoAppFixture, IAsyncLifetime
     {
         private IHost? _host;
         private bool _disposed;
 
-        public HttpServerFixture()
-            : base()
-        {
-            ClientOptions.AllowAutoRedirect = false;
-            ClientOptions.BaseAddress = new Uri("https://localhost");
-            Interceptor = new HttpClientInterceptorOptions().ThrowsOnMissingRegistration();
-        }
-
-        public HttpClientInterceptorOptions Interceptor { get; }
-
-        public ITestOutputHelper? OutputHelper { get; set; }
-
         public string ServerAddress => ClientOptions.BaseAddress.ToString();
 
-        public override IServiceProvider? Services => _host?.Services;
-
-        public void ClearOutputHelper()
-            => OutputHelper = null;
-
-        public void SetOutputHelper(ITestOutputHelper value)
-            => OutputHelper = value;
+        public override IServiceProvider Services => _host?.Services!;
 
         async Task IAsyncLifetime.InitializeAsync()
             => await EnsureHttpServerAsync();
@@ -92,49 +64,13 @@ namespace TodoApp
         {
             base.ConfigureWebHost(builder);
 
-            builder.ConfigureAppConfiguration((builder) =>
-            {
-                string dataDirectory = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-
-                if (!Directory.Exists(dataDirectory))
-                {
-                    Directory.CreateDirectory(dataDirectory);
-                }
-
-                var config = new[]
-                {
-                    KeyValuePair.Create("DataDirectory", dataDirectory),
-                };
-
-                string? directory = Path.GetDirectoryName(typeof(HttpServerFixture).Assembly.Location);
-                string fullPath = Path.Combine(directory ?? ".", "testsettings.json");
-
-                builder.Sources.Clear();
-
-                builder.AddJsonFile(fullPath)
-                       .AddInMemoryCollection(config);
-            });
-
             builder.ConfigureKestrel(
-                (serverOptions) => serverOptions.ConfigureHttpsDefaults(
-                    (httpsOptions) => httpsOptions.ServerCertificate = new X509Certificate2("localhost-dev.pfx", "Pa55w0rd!")));
-
-            builder.ConfigureLogging((loggingBuilder) => loggingBuilder.ClearProviders().AddXUnit(this))
-                   .UseContentRoot(GetApplicationContentRootPath());
-
-            builder.ConfigureServices((services) =>
-            {
-                services.AddSingleton<IHttpMessageHandlerBuilderFilter, HttpRequestInterceptionFilter>(
-                    (_) => new HttpRequestInterceptionFilter(Interceptor));
-
-                services.AddSingleton<IPostConfigureOptions<GitHubAuthenticationOptions>, RemoteAuthorizationEventsFilter>();
-            });
+                serverOptions => serverOptions.ConfigureHttpsDefaults(
+                    httpsOptions => httpsOptions.ServerCertificate = new X509Certificate2("localhost-dev.pfx", "Pa55w0rd!")));
 
             // Configure the server address for the server to
             // listen on for HTTPS requests on a dynamic port.
             builder.UseUrls("https://127.0.0.1:0");
-
-            Interceptor.RegisterBundle("oauth-http-bundle.json");
         }
 
         protected override void Dispose(bool disposing)
@@ -154,38 +90,28 @@ namespace TodoApp
 
         private async Task EnsureHttpServerAsync()
         {
-            if (_host == null)
+            if (_host is null)
             {
-                await CreateHttpServer();
+                await CreateHttpServerAsync();
             }
         }
 
-        private async Task CreateHttpServer()
+        private async Task CreateHttpServerAsync()
         {
-            var builder = CreateHostBuilder().ConfigureWebHost(ConfigureWebHost);
-
-            _host = builder.Build();
+            _host = CreateHostBuilder()!
+                .ConfigureWebHost(ConfigureWebHost)
+                .Build();
 
             // Force creation of the Kestrel server and start it
             var hostedService = _host.Services.GetService<IHostedService>();
             await hostedService!.StartAsync(default);
 
             var server = _host.Services.GetRequiredService<IServer>();
+            var addresses = server.Features.Get<IServerAddressesFeature>();
 
-            ClientOptions.BaseAddress = server.Features.Get<IServerAddressesFeature>()!.Addresses
+            ClientOptions.BaseAddress = addresses!.Addresses
                 .Select((p) => new Uri(p))
-                .First();
-        }
-
-        private string GetApplicationContentRootPath()
-        {
-            var attribute = GetTestAssemblies()
-                .SelectMany((p) => p.GetCustomAttributes<WebApplicationFactoryContentRootAttribute>())
-                .Where((p) => string.Equals(p.Key, "TodoApp", StringComparison.OrdinalIgnoreCase))
-                .OrderBy((p) => p.Priority)
-                .First();
-
-            return attribute.ContentRootPath;
+                .Last();
         }
     }
 }
