8965ff06d27a5d87c1e323831cef9161dc5f8c45
martincostello <martin@martincostello.com>
2021-07-04 15:01:55 +0100

    Fix rebase
    Fix namespaces that got lost during the rebase.

--- src/TodoApp/ApiModule.cs
+++ src/TodoApp/ApiModule.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Martin Costello, 2021. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
+using System.Security.Claims;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
--- tests/TodoApp.Tests/HttpServerFixture.cs
+++ tests/TodoApp.Tests/HttpServerFixture.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 using System.Net.Http;
+using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server;
