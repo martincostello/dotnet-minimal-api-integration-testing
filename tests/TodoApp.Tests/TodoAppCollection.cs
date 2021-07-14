d995ff61d40bd49b2291ceb2207064b31dc7f0b4
martincostello <martin@martincostello.com>
2021-07-14 11:47:52 +0100

    Update .NET SDK
    Update to a newer version of the .NET 6 preview 7 SDK.
    Remove workaround for broken Swagger documentation.
    Add workaround for "preview features" warning.

--- tests/TodoApp.Tests/TodoAppCollection.cs
+++ tests/TodoApp.Tests/TodoAppCollection.cs
@@ -3,9 +3,11 @@
 
 namespace TodoApp
 {
+#pragma warning disable CA2252 // HACK Appears to be a bug - investigate in a later preview build
     [CollectionDefinition(Name)]
     public sealed class TodoAppCollection : ICollectionFixture<TodoAppFixture>
     {
         public const string Name = "TodoApp server collection";
     }
+#pragma warning restore CA2252
 }
