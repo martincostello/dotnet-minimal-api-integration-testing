4ef079a2f348d12548afa78e8266b8cb4671224e
martincostello <martin@martincostello.com>
2021-07-29 18:41:36 +0100

    Fix spacing
    Add a new line between the namespace and the class declaration.

--- src/TodoApp/Data/TodoItem.cs
+++ src/TodoApp/Data/TodoItem.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
 namespace TodoApp.Data;
+
 public class TodoItem
 {
     public Guid Id { get; set; }
