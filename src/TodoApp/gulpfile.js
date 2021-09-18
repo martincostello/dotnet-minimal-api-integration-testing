bb0b42cdd7635a2b84f5c283fcc86c1d9ba3e25a
martincostello <martin@martincostello.com>
2021-09-18 14:33:12 +0100

    Add watch task
    Add a watch task for gulp.

--- src/TodoApp/gulpfile.js
+++ src/TodoApp/gulpfile.js
@@ -7,9 +7,10 @@ let source = require('vinyl-source-stream');
 let sourcemaps = require('gulp-sourcemaps');
 let tsify = require('tsify');
 let uglify = require('gulp-uglify');
+var sourceFiles = ['scripts/ts/**/*.ts'];
 
 gulp.task('lint', function () {
-    return gulp.src(['scripts/ts/**/*.ts'])
+    return gulp.src(sourceFiles)
         .pipe(eslint({
             formatter: 'visualstudio'
         }))
@@ -49,3 +50,7 @@ gulp.task('build', function () {
 });
 
 gulp.task('default', gulp.series('lint', 'build'));
+
+gulp.task('watch', function () {
+    gulp.watch(sourceFiles, gulp.series('default'));
+});
