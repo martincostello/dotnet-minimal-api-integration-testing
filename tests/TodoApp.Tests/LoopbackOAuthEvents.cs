9f2c1c00d82e3cbf71e79d833cc21a543feb7998
martincostello <martin@martincostello.com>
2021-09-24 16:07:41 +0100

    Use custom OAuthEvents class
    Use a custom OAuthEvents class and move the loopback implementation there.

---
+++ tests/TodoApp.Tests/LoopbackOAuthEvents.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Martin Costello, 2021. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+using System.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+
+namespace TodoApp;
+
+public sealed class LoopbackOAuthEvents : OAuthEvents
+{
+    public override Task RedirectToAuthorizationEndpoint(RedirectContext<OAuthOptions> context)
+    {
+        // Extract the state and return URI from the intended
+        // destination the browser would be directed to.
+        var query = new UriBuilder(context.RedirectUri).Uri.Query;
+        var queryString = HttpUtility.ParseQueryString(query);
+
+        var location = queryString["redirect_uri"];
+        var state = queryString["state"];
+
+        queryString.Clear();
+
+        // Redirect the browser back to the test application with
+        // the state from the original intended destination.
+        var code = Guid.NewGuid().ToString();
+
+        queryString.Add("code", code);
+        queryString.Add("state", state);
+
+        var builder = new UriBuilder(location!)
+        {
+            Query = queryString.ToString() ?? string.Empty
+        };
+
+        context.RedirectUri = builder.ToString();
+
+        return base.RedirectToAuthorizationEndpoint(context);
+    }
+}
--- tests/TodoApp.Tests/RemoteAuthorizationEventsFilter.cs
+++ tests/TodoApp.Tests/RemoteAuthorizationEventsFilter.cs
@@ -1,10 +1,7 @@
 ﻿// Copyright (c) Martin Costello, 2021. All rights reserved.
 // Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
 
-using System.Collections.Specialized;
-using System.Web;
 using AspNet.Security.OAuth.GitHub;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
 
 namespace TodoApp;
@@ -26,49 +23,6 @@ public sealed class RemoteAuthorizationEventsFilter : IPostConfigureOptions<GitH
 
         // Configure the GitHub provider to redirect back to the
         // test application, rather than GitHub's own login pages.
-        options.Events.OnRedirectToAuthorizationEndpoint = RedirectToSelfForOAuth;
-    }
-
-    private static Task RedirectToSelfForOAuth<T>(RedirectContext<T> context)
-        where T : AuthenticationSchemeOptions
-    {
-        // Extract the state and return URI from the intended
-        // destination the browser would be directed to.
-        var queryString = ParseQueryString(context);
-
-        var location = queryString["redirect_uri"];
-        var state = queryString["state"];
-
-        queryString.Clear();
-
-        // Redirect the browser back to the test application with
-        // the state from the original intended destination.
-        var code = Guid.NewGuid().ToString();
-
-        queryString.Add("code", code);
-        queryString.Add("state", state);
-
-        var builder = new UriBuilder(location!)
-        {
-            Query = queryString.ToString() ?? string.Empty
-        };
-
-        return Redirect(context, builder);
-    }
-
-    private static NameValueCollection ParseQueryString<T>(RedirectContext<T> context)
-        where T : AuthenticationSchemeOptions
-    {
-        return HttpUtility.ParseQueryString(new UriBuilder(context.RedirectUri).Uri.Query);
-    }
-
-    private static Task Redirect<T>(RedirectContext<T> context, UriBuilder builder)
-        where T : AuthenticationSchemeOptions
-    {
-        var location = builder.Uri.ToString();
-
-        context.Response.Redirect(location);
-
-        return Task.CompletedTask;
+        options.EventsType = typeof(LoopbackOAuthEvents);
     }
 }
