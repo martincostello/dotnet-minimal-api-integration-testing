a0fdb7ca7770f7ab1fd1a9856d7f4ada07c825a2
martincostello <martin@martincostello.com>
2022-01-24 15:01:42 +0000

    Refactor UI tests
    Apply some of the refactoring from https://github.com/martincostello/dotnet-playwright-tests to the test fixture used in the sample.

---
+++ tests/TodoApp.Tests/BrowserFixtureOptions.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Martin Costello, 2021. All rights reserved.
+// Licensed under the Apache 2.0 license. See the LICENSE file in the project root for full license information.
+
+namespace TodoApp;
+
+public class BrowserFixtureOptions
+{
+    public string BrowserType { get; set; } = Microsoft.Playwright.BrowserType.Chromium;
+
+    public string? BrowserChannel { get; set; }
+
+    public string? TestName { get; set; }
+}
